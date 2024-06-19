a8b3b17a2986b01a22f0cfb3d93d263be36f50c4
HexDexDev <138143730+HexDexDev@users.noreply.github.com>
2024-06-19 14:03:09 -0300

    Second commit

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ let mensagemDeErro = "Erro! Preencha todos os campos.";
 alert(mensagemDeErro);
 
 // prompt para perguntar o nome do usuário e armazená-lo na variável nome
-let nome = prompt("Qual o seu nome?");
+nome = prompt("Qual é o seu nome?");
 
 // Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
-let idade = prompt("Qual a sua idade?");
+idade = prompt("Qual é a sua idade?");
 
 // Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
 if (idade >= 18){
